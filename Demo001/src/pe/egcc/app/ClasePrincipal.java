08197cbdfd916330d3faa2665164a7b403a1780e
carlostorres1 <geancarlos_19.cdf@hotmail.com>
2016-04-24 18:06:42 -0500

    prueba giob

---
+++ Demo001/src/pe/egcc/app/ClasePrincipal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.egcc.app;
+
+import pe.egss.app.view.SumaView;
+
+public class ClasePrincipal {
+    
+    
+    
+    public static void main(String[] args) {
+        SumaView.main(null);
+    }
+}
---
+++ Demo001/src/pe/egss/app/service/MateService.java
@@ -0,0 +1,8 @@
+
+
+package pe.egss.app.service;
+public class MateService {
+    public int sumar(int n1 ,int n2){
+        return (n1+n2);
+    }
+}
